8ff941c699e3f9a713d2173c3ed2e4644a8e9734
Kevin Hann <kebu.hann@gmail.com>
2020-08-08 15:58:32 +0300

    more config

--- index.ts
+++ index.ts
@@ -3,37 +3,36 @@ import cors from "cors"
 import mongoose from "mongoose"
 import { MONGODB_URI, PORT } from "./utils/config"
 import http from "http"
-import { Test } from "./models/test"
 import bodyParser from "body-parser"
+import { testRouter } from "./routes/test"
 
+// Mongo
 mongoose.set("useCreateIndex", true)
 const connect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://TBDAdmin:Troxta2xGxkiG5Wb@tbdcluster.rnme5.mongodb.net/TBDMongo?retryWrites=true&w=majority",
+      `${MONGODB_URI}`,
       {
         useNewUrlParser: true
       }
     )
     console.log("Connected to mongodb")
   } catch (exception) {
-    console.error("error connecting to MongoDB:", exception.message)
-  }
+            console.error("error connecting to MongoDB:", exception.message)
+              }
 }
 connect()
 
 const app = express()
-
+// Middlewares
 app.use(express.static("build"))
-
 app.use(cors())
 app.use(bodyParser())
-app.get("/ping", (_req, res) => res.send("pong"))
-app.get("/test", async (_req, res) => {
-  console.log("test")
 
-  res.json(await Test.findOne({}))
-})
+// Routes
+app.use(testRouter)
+
+// Server
 const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log(`App runnin on port ${PORT}`)
---
+++ routes/test.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import {Test} from '../models/test'
+export const testRouter = express.Router()
+
+testRouter.get("/test", async (_req, res) => {
+    res.json(await Test.findOne({}))
+})                    
+testRouter.get("/ping", (_req, res) => res.json({time : new Date(), message:"pong"}))
